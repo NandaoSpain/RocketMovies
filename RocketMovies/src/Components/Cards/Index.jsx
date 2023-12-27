8d7677cab16fc90307a6392bf5731eef7d03cc78
Fernando Rodrigues <fernando111oficina@gmail.com>
2023-12-27 19:43:02 +0100

    star

--- RocketMovies/src/Components/Cards/Index.jsx
+++ RocketMovies/src/Components/Cards/Index.jsx
@@ -3,7 +3,10 @@ import { Container } from "./styles";
 export function Cards() {
     return(
         <Container>
-            <h1>Interestellar</h1>
+            <div>
+                <h1>Interestellar</h1>
+
+            </div>
             <p>As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial, possibilitando a continuação da espécie. Cooper é chamado para liderar o grupo e aceita a missão sabendo que pode nunca mais ver os filhos. Ao lado de Brand, Jenkins e Doyle, ele seguirá em busca de um novo lar.
 </p>
         </Container>
---
+++ RocketMovies/src/Components/Star/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export function Star() {
+    return(
+        <Container>
+            
+        </Container>
+    );
+}
